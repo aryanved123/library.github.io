7a307756222d2999d08fd8d0e9f2447d8f517f70
Aryan Ved <113310577+aryanved123@users.noreply.github.com>
2025-02-02 17:26:22 -0500

    Delete bookReviewServer/src/main directory

--- bookReviewServer/src/main/java/com/example/bookreview/BookApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookreview;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class BookApplication extends Application {
-}
